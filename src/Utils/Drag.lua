108dabdd49d86c8f314494d7d99405225c1170ef
1camroach12-dotcom <1cam.roach12@gmail.com>
2026-05-24 03:16:30 -0400

    Add Drag.lua - draggable util

---
+++ src/Utils/Drag.lua
@@ -0,0 +1,37 @@
+--[[
+    WSscripts UI Library - Drag Util
+    Author: WSscripts Team
+--]]
+local UIS = game:GetService("UserInputService")
+
+local Drag = {}
+Drag.__index = Drag
+
+function Drag:Draggable(frame, dragBar)
+    local dragging = false
+    local dragStart, startPos
+
+    dragBar.InputBegan:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 then
+            dragging = true
+            dragStart = input.Position
+            startPos = frame.Position
+        end
+    end)
+    dragBar.InputEnded:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 then
+            dragging = false
+        end
+    end)
+    UIS.InputChanged:Connect(function(input)
+        if dragging and input.UserInputType == Enum.UserInputType.MouseMovement then
+            local delta = input.Position - dragStart
+            frame.Position = UDim2.new(
+                startPos.X.Scale, startPos.X.Offset + delta.X,
+                startPos.Y.Scale, startPos.Y.Offset + delta.Y
+            )
+        end
+    end)
+end
+
+return Drag
